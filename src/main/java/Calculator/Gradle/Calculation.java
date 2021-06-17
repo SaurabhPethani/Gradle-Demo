d1ebe16ff5ac0db5ca7b9316c7da66216edd7b68
Saurabh <saurabh.p@emeasurematics.com>
2021-06-17 18:04:09 +0530

    Unit test done

---
+++ src/main/java/Calculator/Gradle/Calculation.java
@@ -0,0 +1,5 @@
+package Calculator.Gradle;
+
+public interface Calculation {
+    double operation(double a, double b);
+}
---
+++ src/main/java/Calculator/Gradle/Multiplication.java
@@ -0,0 +1,8 @@
+package Calculator.Gradle;
+
+public class Multiplication implements  Calculation{
+    @Override
+    public double operation(double a, double b) {
+        return a*b;
+    }
+}
